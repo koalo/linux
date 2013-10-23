93cfed1d3477b35be1f9bd375d73c044e2fc2d78
Wojciech Zabołotny <wzab@WZlap.nasz.dom>
2013-10-23 20:14:56 +0200

    Added codec as a pltaform device

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -728,6 +728,13 @@ static struct platform_device snd_rpi_cs534x_device = {
 	.id = 0,
 	.num_resources = 0,
 };
+
+static struct platform_device snd_rpi_cs534x_codec_device = {
+	.name = "cs534x-codec",
+	.id = -1,
+	.num_resources = 0,
+};
+
 #endif
 
 #ifdef CONFIG_SND_BCM2708_SOC_RPI_CODEC_ESS9018_MODULE
